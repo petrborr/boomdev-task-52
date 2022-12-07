b69f7cc258b0c84efa9aa1f9f624d08a17a277c3
petrborr <petrborr@gmail.com>
2022-12-07 23:29:41 +0200

    add: 5 article tags to body

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
+    for (let i = 0; i < 5; i++) {
+      const article = document.createElement("article")
+      article.classList.add("message")
+      article.innerText = "sample content"
+      document.body.appendChild(article)
+    }
     alert("💣");
   });
 });
